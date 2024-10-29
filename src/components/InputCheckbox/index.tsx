9cdb72df94edba09b3f09545d8778379d40c423c
Nélio Espíndula Junior <nelio.espindula@zig.fun>
2024-10-29 19:33:55 -0300

    Bug #2: Approve checkbox not working
    Signed-off-by: Nélio Espíndula Junior <nelio.espindula@zig.fun>

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -5,9 +5,15 @@ import { InputCheckboxComponent } from "./types"
 export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
   const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
 
+  /*
+  The issue was that the browser wasn't automatically linking the input to the label.
+  We can fix this by using the `htmlFor` attribute, as I did, or by wrapping the input
+  tag within the label element, which will also work.
+  */
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
+        htmlFor={inputId}
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
